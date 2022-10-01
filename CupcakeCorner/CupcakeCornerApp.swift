f41914a65c46192d0d01e41d0aa99682e8950fa7
Justin Hold <36420615+jhold1982@users.noreply.github.com>
2022-09-30 21:05:58 -0500

    Initial Commit

---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Justin Hold on 9/30/22.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
